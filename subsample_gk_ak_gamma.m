6383d7db662c47e5756c000ad9dd4f81fd33550d
stefan <hoitaus@gmail.com>
2013-03-15 10:25:10 +1100

    start working on subsampling and plotting scripts

---
+++ subsample_gk_ak_gamma.m
@@ -0,0 +1,31 @@
+% subsample and convert sp to sa
+clear all
+
+% Load selected variables.
+vars = {'s','ct','p','lats','longs'};
+load('/home/z3439823/mymatlab/omega/data_paul/gk_ak_gamma.mat', vars{:})
+
+sk=4;
+xskip=sk;
+yskip=sk;
+zskip=sk;
+
+% subsample
+s=s(1:zskip:end,1:yskip:end,1:xskip:end);
+ct=ct(1:zskip:end,1:yskip:end,1:xskip:end);
+p=p(1:zskip:end,1:yskip:end,1:xskip:end);
+lat=lats(1:yskip:end);
+lon=longs(1:xskip:end);
+
+ss=size(s);
+lon=repmat(permute(lon,[3,2,1]),[ss(1),ss(2),1]);
+lat=repmat(permute(lat,[3,1,2]),[ss(1),1,ss(3)]);
+    
+% convert sp to sa
+s=gsw_SA_from_SP(s,p,lon,lat);
+
+lats=lat;
+longs=lon;
+
+save('/home/z3439823/mymatlab/omega/data_stefan/gk_ak_gamma_subs4x4.mat',vars{:})
+
