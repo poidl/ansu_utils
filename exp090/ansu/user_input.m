9e8324dd919ca9a3404c2adac0d0088c84b91f00
stefan <hoitaus@gmail.com>
2013-06-27 09:43:17 +1000

    Thu Jun 27 09:43:21 EST 2013

---
+++ exp090/ansu/user_input.m
@@ -0,0 +1,18 @@
+zonally_periodic=true;
+choice= 'epsilon'; % density gradient error
+solver = 'iterative';
+nit = 21;
+
+save_iterations=true; % save variables for postprocessing 
+history_file='data/iteration_history.mat'; % absolute path or path relative to the topmost calling function 
+
+% TODO: the following should be set somewhere else
+initial_surface_at_constant_pressure=false;
+if initial_surface_at_constant_pressure;
+    initial_pressure=1e3;
+    nlevels=length(initial_pressure);
+else
+    glevels=[26.8];
+    p_r=0; % reference pressure for initial surface
+    nlevels=length(glevels);
+end
